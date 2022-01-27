32a7b1bb74ed0362d675a52a979b95a1f551d5d7
balamurugan213 <balainfo21@gmail.com>
2022-01-27 11:43:14 +0530

    Implementing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/webcredentials.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: kIsWeb ? firebaseConfig : null,
+  );
   runApp(const MyApp());
 }
 
